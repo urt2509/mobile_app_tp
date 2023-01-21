80394957949be8bbb1e0be803198f0c2af750e06
urt2509 <99096447+urt2509@users.noreply.github.com>
2023-01-21 20:38:50 +0000

    Deploying to gh-pages from @ urt2509/mobile_app_tp@3a56f57b233d1ad733dff454b70e2c37d6da604e 🚀

---
+++ index.48df5b42.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.48df5b42.js.map
---
+++ index.58773b1e.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.58773b1e.js.map
